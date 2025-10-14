629273f8049b12756d084fe73e925542120c4dcb
Amritansu Aditya <amritansuaditya1@gmail.com>
2025-10-14 21:58:28 +0530

    updated navbar changed anchor dag to navigator

--- frontend/src/pages/components/common/NavBar.jsx
+++ frontend/src/pages/components/common/NavBar.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import logo from '../../../assets/images/logo.jpg';
 import './NavBar.css';
+import { useNavigate } from 'react-router-dom';
 
-const NavBar = () => {
+const NavBar = (props) => {
+  const navigate = useNavigate();
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleMenu = () => {
@@ -12,14 +14,16 @@ const NavBar = () => {
   return (
     <div className='header'>
       <div className='logo'>
-        <a href='/'> <img src={logo} alt="Logo" /> </a>
+        <button onClick={() => navigate('/')} style={{ background: 'none', border: 'none', padding: 0, cursor: 'pointer' }}>
+          <img src={logo} alt="Logo" />
+        </button>
       </div>
       <nav className={`nav ${isOpen ? 'open' : ''}`}>
         <ul>
-          <li><a href='/'>Home</a></li>
-          <li><a href='/news'>News</a></li>
-          <li><a href='/career'>Career</a></li>
-          <li><a href='/aktuprep'>Exam</a></li>
+          <li><button onClick={() => navigate('/')}>Home</button></li>
+          <li><button onClick={() => navigate('/news')}>News</button></li>
+          <li><button onClick={() => navigate('/career')}>Career</button></li>
+          <li><button onClick={() => navigate('/aktuprep')}>Exam</button></li>
         </ul>
       </nav>
       <button className="hamburger" onClick={toggleMenu}>
